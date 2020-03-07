0ea04b0029e70f8a85bc8e179a3a8b5e7b414571
Paine Leffler <pleffler@vols.utk.edu>
2020-03-07 07:49:53 -0600

    catch all express route 🎣

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
